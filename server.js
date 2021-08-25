3b3bd75a7f792e8e2f820d9b1c602467b5b4341b
plaurion1989 <plaurion@protonmail.com>
2021-08-25 13:45:46 -0700

    finished lab 07

--- server.js
+++ server.js
@@ -14,20 +14,29 @@ app.get('/weather', (req,res) => {
   let lat = req.query.lat;
   let lon = req.query.lon;
   let searchQuery = req.query.searchQuery;
+  //console the function variables
   console.log(lat,lon,searchQuery);
+  //
   let locationWeatherData = weatherData.find(location => location.city_name === searchQuery);
+  //
   if (locationWeatherData === undefined){
     res.status(400).send('no weather for this location');
   }else {
-    let locationRefactored = locationWeatherData.data.map(obj => new Forecast(obj.datetime, `Low of ${obj.low_temp}, a High of ${obj.max_temp} with ${obj.weather.description}`));
-    res.send(locationRefactored);
+    // let locationRefactored = locationWeatherData.data.map(day => new Forecast(day)
+    //   day.datetime, `Low of ${day.low_temp}, a High of ${day.max_temp} with ${day.weather.description}`
+    // );
+    // res.send(locationRefactored);
+    const forcasts = locationWeatherData.data.map(day => {
+      return new Forecast(day);
+    });
+    res.send(forcasts);
   }
 });
 
 class Forecast {
-  constructor(date,description) {
-    this.date = date;
-    this.description= description;
+  constructor(day) {
+    this.description= `Low of ${day.low_temp}, High of ${day.high_temp} with ${day.weather.description}`;
+    this.date = day.datetime;
   }
 }
 
@@ -35,7 +44,7 @@ class Forecast {
 
 
 
-app.get('/*', (req,res) => {
-  res.status(404).send('path not found');
+app.get('*', (req,res) => {
+  res.status(500).send('path not found');
 });
 app.listen(PORT, () => {console.log(`listening on port ${PORT}`);});
