e1312dc4fdf9d61f7d9c3e644e79a7bbd72d4680
plaurion1989 <plaurion@protonmail.com>
2021-08-25 17:15:46 -0700

    finished two cards in trello. acp for testing

--- server.js
+++ server.js
@@ -1,50 +1,65 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-require ('dotenv').config();
-const cors = require ('cors');
+require('dotenv').config();
+const cors = require('cors');
 const PORT = process.env.PORT || 3002;
 app.use(cors());
-const weatherData = require('./data/weather.json');
-
-app.get('/', (req,res)=> {
-  res.send('server is working');
-});
-
-app.get('/weather', (req,res) => {
-  let lat = req.query.lat;
-  let lon = req.query.lon;
-  let searchQuery = req.query.searchQuery;
-  //console the function variables
-  console.log(lat,lon,searchQuery);
-  //
-  let locationWeatherData = weatherData.find(location => location.city_name === searchQuery);
-  //
-  if (locationWeatherData === undefined){
-    res.status(400).send('no weather for this location');
-  }else {
-    // let locationRefactored = locationWeatherData.data.map(day => new Forecast(day)
-    //   day.datetime, `Low of ${day.low_temp}, a High of ${day.max_temp} with ${day.weather.description}`
-    // );
-    // res.send(locationRefactored);
-    const forcasts = locationWeatherData.data.map(day => {
-      return new Forecast(day);
-    });
-    res.send(forcasts);
-  }
-});
+const axios = require('axios');
 
 class Forecast {
   constructor(day) {
-    this.description= `Low of ${day.low_temp}, High of ${day.high_temp} with ${day.weather.description}`;
+    this.description = `Low of ${day.low_temp}, High of ${day.high_temp} with ${day.weather.description}`;
     this.date = day.datetime;
   }
 }
+class Movie {
+  constructor(movie) {
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.vote_average = movie.vote_average;
+    this.vote_count = movie.vote_count;
+    this.image_url = movie.poster_path;
+    this.release_date = movie.release_date;
+  }
+}
 
+app.get('/', (req, res) => {
+  res.send('server is working');
+});
 
+app.get('/weather', async (req, res) => {
+  let lat = req.query.lat;
+  let lon = req.query.lon;
 
+  let weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`);
+  //error message
+  if (weatherData === undefined) {
+    res.status(400).send('location not found');
+  } else {
+    // weatherData.data.data is path to object
+    let forecast = weatherData.data.data.map(day => {
+      return new Forecast(day)});
+    res.send(forecast);
+  }
+});
 
+app.get('/movies', async (req, res) => {
+  let searchQuery= req.query.searchQuery;
+  let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
+  //error message
+  if (movieData === undefined) {
+    res.status(400).send('location not found');
+  } else {
+    // weatherData.data.data is path to object
+    console.log(movieData.data);
+    let movies = movieData.data.results.map(movie => {
+      return new Movie(movie)});
+    res.send(movies);
+  }
+});
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.status(500).send('path not found');
 });
-app.listen(PORT, () => {console.log(`listening on port ${PORT}`);});
+
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`); });
